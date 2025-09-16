a6643d2f091e79737194858d9b1a50e5d0d71aa8
Miguel Guadarrama <miguelguadarrama@gmail.com>
2025-09-16 21:44:44 +0300

    default photo while waiting url

--- components/ui/studentPhoto.tsx
+++ components/ui/studentPhoto.tsx
@@ -10,7 +10,6 @@ const StudentPhoto = ({
 	style?: any
 }) => {
 	const [url, setUrl] = useState<string | null>(null)
-	const [notFound, setNotFound] = useState(false)
 	const base_url = process.env.EXPO_PUBLIC_STORAGE_URL
 
 	const gender = student.gender_alias === "gender_female" ? "girl" : "boy"
@@ -21,37 +20,28 @@ const StudentPhoto = ({
 		}
 
 		if (student.id) {
-			fetchPhoto(student.id)
-				.then((response) => {
-					if (response.ok) {
-						setUrl(`${base_url}/app/students/${student.id}.jpg`)
-					} else {
-						setNotFound(true)
-					}
-				})
-				.catch(() => {
-					setNotFound(true)
-				})
+			fetchPhoto(student.id).then((response) => {
+				if (response.ok) {
+					setUrl(`${base_url}/app/students/${student.id}.jpg`)
+				}
+			})
 		}
 	}, [student])
 
-	if (notFound === true) {
-		return (
-			<Image
-				source={
-					gender === "boy"
-						? require("../../assets/boy-placeholder.png")
-						: require("../../assets/girl-placeholder.png")
-				}
-				style={style || {}}
-			/>
-		)
+	if (url !== null) {
+		return <Image source={{ uri: url }} style={style || {}} />
 	}
 
-	if (url === null) {
-		return null
-	}
-	return <Image source={{ uri: url }} style={style || {}} />
+	return (
+		<Image
+			source={
+				gender === "boy"
+					? require("../../assets/boy-placeholder.png")
+					: require("../../assets/girl-placeholder.png")
+			}
+			style={style || {}}
+		/>
+	)
 }
 
 export default StudentPhoto
